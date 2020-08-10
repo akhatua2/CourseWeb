3df55a3666fcee614a905649da471cd6910f7a43
Arpandeep Khatua <akhatua2@illinois.edu>
2020-08-10 23:37:03 +0400

    Updates

---
+++ precache-manifest.907709287a3831d8ff5ef7f72b567134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f021eb878b8bf238060697b449b5bed",
+    "url": "/CourseWeb/index.html"
+  },
+  {
+    "revision": "b262f5cca159ca8ec455",
+    "url": "/CourseWeb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e82d7ed773ed0fec8ed1",
+    "url": "/CourseWeb/static/js/2.51261611.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CourseWeb/static/js/2.51261611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b262f5cca159ca8ec455",
+    "url": "/CourseWeb/static/js/main.ad5904cf.chunk.js"
+  },
+  {
+    "revision": "6deeb1b30df0edd76406",
+    "url": "/CourseWeb/static/js/runtime-main.6c7fd898.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CourseWeb/static/media/logo.5d5d9eef.svg"
+  }
+]);
